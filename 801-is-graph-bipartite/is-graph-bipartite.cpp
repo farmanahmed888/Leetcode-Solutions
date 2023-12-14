b179d3905b2b8839074acf5e8a05a413be645fa2
Farman Ahmed <farmanahmed888@gmail.com>
2023-12-14 18:09:54 +0530

    Time: 19 ms (67.13%) | Memory: 14.1 MB (35.71%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    bool helper(int start,vector<vector<int>>&graph){
-        int n=graph.size();
-       vector<int>color(n,-1);
-       int c=1;
-       queue<int>q;
-       q.push(start);
-       color[start]=c;
-       while(!q.empty()){
-           int x=q.front();q.pop();
-           for(auto it:graph[x]){
-               if(color[it]==-1){
-                   color[it]=1-color[x];
-                   q.push(it);
-               }else if(color[it]==color[x]) return false;
-           }
-       }
-        return true;
-    }
     bool isBipartite(vector<vector<int>>& graph) {
-       int n=graph.size();
-       for(int i=0;i<n;i++){
-           if(!helper(i,graph)){
-               return false;
-           }
-       }
-       return true;
+        int n=graph.size();
+        vector<int>color(n,0);
+        for(int i=0;i<n;i++){
+            if(color[i]!=0) continue;
+            queue<int>q;
+            q.push(i);
+            color[i]=1;
+            while(!q.empty()){
+                int x=q.front();q.pop();
+                for(auto it:graph[x]){
+                    if(color[it]==0){
+                        color[it]=-1*color[x];
+                        q.push(it);
+                    }else if(color[it]==color[x]) return false;
+                }
+            }
+        }
+        return true;
     }
 };
